dd3972e2dda43db862dc657553e19bd3086bd3b7
Datior <leonardowederveiga@yahoo.com>
2023-03-06 12:47:56 -0300

    Aprofundando em promises

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
+var consultaCep = fetch('https://viacep.com.br/ws/01001230/json/')
+    .then(resposta => resposta.json())
+    .then(r => {
+        if (r.erro) {
+            throw Error("CEP não encontrado!")
+        } else 
+        console.log(r)
+    })
+    .catch(erro => console.log(erro))
+    .finally(mensagem => console.log("Bloco  finalizado!!!"))
+
+
 
 console.log(consultaCep)
